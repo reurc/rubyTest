ad315dcfa2cf301d3e158340806164cc51594f1e
reurc <ricardo.escobaru@gmail.com>
2018-07-24 00:17:33 -0500

    Update message with current time

--- myRailsProject/app/controllers/application_controller.rb
+++ myRailsProject/app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
+require 'date'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, Jeannine!"
+    currentTime = Time.now.utc
+    render html: "hello, Jeannine! the time is now #{currentTime.hour}:#{currentTime.min} "
   end
 end
